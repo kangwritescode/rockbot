a290157994654c85a9ea232c27481deecf475e5e
kangwritescode <kangwritescode@gmail.com>
2020-02-21 12:27:21 -0800

    added navbar and navbar__button styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
 import RockHeader from './components/RockHeader/RockHeader'
+import NavBar from './components/NavBar/NavBar'
 
 function App () {
   return (
     <React.Fragment>
       <RockHeader />
+      <NavBar />
     </React.Fragment>
   )
 }
